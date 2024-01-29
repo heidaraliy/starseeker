dc1f12c20578578dc62e94735cc324c6b9311fa3
Heidar Aliy <heidaraliy@Heidars-MacBook-Air.local>
2024-01-28 19:47:14 -0500

    session bug found and fixed

--- backend/go/services/session/userSessionSignInService.go
+++ backend/go/services/session/userSessionSignInService.go
@@ -4,6 +4,7 @@ import (
 	"backend/go/models"
 	"backend/go/repositories"
 	"context"
+	"errors"
 	"log"
 )
 
@@ -19,13 +20,19 @@ func (s *SessionSignInService) CreateNewUserSession(ctx context.Context, session
 	log.Println("Checking for existing user session for user:", user.Auth0ID.String)
 
 	existingUserSession, err := s.sessionRepo.FindSessionByAuth0ID(ctx, user.Auth0ID.String)
-	log.Printf("User session details: %v", existingUserSession)
-	if existingUserSession.Status == "TERMINATED" {
+	if err != nil {
+		if errors.Is(err, repositories.ErrSessionNotFound) {
+			log.Println("No active session found, creating a new one for:", user.Email)
+			return s.sessionRepo.CreateUserSession(ctx, &session, &user)
+		} else {
+			log.Printf("Error occured.")
+			return err
+		}
+	}
+
+	if existingUserSession == nil || existingUserSession.Status == "TERMINATED" {
 		log.Println("No active session found, creating a new one for:", user.Email)
 		return s.sessionRepo.CreateUserSession(ctx, &session, &user)
-	} else if err != nil {
-		log.Printf("Error when attempting to find user session: %v", err)
-		return err
 	} else if existingUserSession.Status == "ACTIVE" {
 		log.Printf("Existing session found for user: %s, session ID: %s", user.Email, existingUserSession.SessionID)
 		log.Printf("Retrieved session status: %s", existingUserSession.Status)
@@ -33,5 +40,6 @@ func (s *SessionSignInService) CreateNewUserSession(ctx context.Context, session
 
 		return nil
 	}
+
 	return nil
 }
